a3690c77429b73c31a980712107af38da709591a
123XiaoShan123 <108461624+123XiaoShan123@users.noreply.github.com>
2023-06-23 21:17:08 +0800

    Add files via upload

---
+++ axi_stream_insert_header.v
@@ -0,0 +1,281 @@
+module axi_stream_insert_header #(
+    parameter DATA_WD = 32,
+    parameter DATA_BYTE_WD = DATA_WD / 8,
+    parameter BYTE_CNT_WD = $clog2(DATA_BYTE_WD)
+) (
+    input clk,
+    input rst_n,
+    // AXI Stream input original data
+    input valid_in,
+    input [DATA_WD-1 : 0] data_in,
+    input [DATA_BYTE_WD-1 : 0] keep_in,
+    input last_in,
+    output ready_in,
+    // AXI Stream output with header inserted
+    output valid_out,
+    output [DATA_WD-1 : 0] data_out,
+    output [DATA_BYTE_WD-1 : 0] keep_out,
+    output last_out,
+    input ready_out,
+    // The header to be inserted to AXI Stream input
+    input valid_insert,
+    input [DATA_WD-1 : 0] data_insert,
+    input [DATA_BYTE_WD-1 : 0] keep_insert,
+    input [BYTE_CNT_WD-1 : 0] byte_insert_cnt,
+    output ready_insert
+);
+// Your code here
+
+reg [DATA_WD - 1 : 0]               data_insert_r;
+reg [DATA_BYTE_WD - 1 : 0]          keep_insert_r;
+reg [BYTE_CNT_WD-1 : 0]             byte_insert_cnt_r;
+
+reg [DATA_WD - 1 : 0]               data_in_r;
+reg                                 last_in_r;
+reg [DATA_BYTE_WD - 1 : 0]          keep_in_r;
+
+reg                                 last_out_r;
+reg [DATA_WD - 1 : 0]               data_out_r;
+reg                                 valid_out_r;
+reg [DATA_BYTE_WD - 1 : 0]          keep_out_r;
+
+reg                                 ready_insert_r;
+
+/*-----------------------------------------------------header_insert-----------------------------------------------*/
+assign ready_insert = ready_out && ready_insert_r; //&& ready_insert_r);                           
+
+always @(posedge clk or negedge rst_n) begin
+    if(!rst_n)begin
+        data_insert_r     <= 0;
+        keep_insert_r     <= 0;
+        byte_insert_cnt_r <= 0;
+        ready_insert_r    <= 1;
+    end
+    else if(ready_out)begin
+        if(valid_in&&valid_insert)begin
+            if(last_in_r || (keep_insert != keep_insert_r))begin                                            //第一个数据用keep_insert判断截取方式，将data_insert有效字节存入data_insert_r
+                case(keep_insert)
+                    4'b0001:begin
+                    data_insert_r <= {{3{8'h00}},data_insert[7 : 0]};
+                    end
+                    4'b0011:begin
+                    data_insert_r <= {{2{8'h00}},data_insert[15 : 0]};
+                    end
+                    4'b0111:begin
+                    data_insert_r <= {{8'h00},data_insert[23 : 0]};
+                    end
+                    4'b1111:begin
+                    data_insert_r <= {data_insert[31 : 0]};
+                    end
+                    default:begin
+                    data_insert_r <= {data_insert};
+                    end
+                endcase
+                keep_insert_r     <= keep_insert;
+                byte_insert_cnt_r <= byte_insert_cnt;
+                ready_insert_r    <= 0;
+            end
+            else begin                                                       //之后数据用keep_insert_r判断截取方式，将data_in_r有效字节存入data_insert_r
+                case(keep_insert_r)
+                    4'b0001:begin
+                    data_insert_r <= {{3{8'h00}},data_in_r[7 : 0]};
+                    end
+                    4'b0011:begin
+                    data_insert_r <= {{2{8'h00}},data_in_r[15 : 0]};
+                    end
+                    4'b0111:begin
+                    data_insert_r <= {{8'h00},data_in_r[23 : 0]};
+                    end
+                    4'b1111:begin
+                    data_insert_r <= {data_in_r[31 : 0]};
+                    end
+                    default:begin
+                    data_insert_r <= data_insert_r;
+                    end
+                endcase
+                keep_insert_r     <= keep_insert_r;
+                byte_insert_cnt_r <= byte_insert_cnt_r;
+                ready_insert_r    <= ready_insert_r;
+            end
+        end 
+        else begin                                                          //valid_insert=1 判断一笔data_insert传输完成，复位insert
+            keep_insert_r     <= 0;
+            data_insert_r     <= 0;
+            byte_insert_cnt_r <= 0;
+            ready_insert_r    <= 1;
+        end
+    end
+    else begin                                                             //中断传输，立即缓存当前数据
+        keep_insert_r     <= keep_insert_r;
+        data_insert_r     <= data_insert_r;
+        byte_insert_cnt_r <= byte_insert_cnt_r;
+        ready_insert_r    <= ready_insert_r;
+    end
+end
+
+/*-----------------------------------------------------data_in-------------------------------------------------------*/
+assign ready_in = ready_out;
+
+always @(posedge clk or negedge rst_n) begin
+    if(!rst_n)begin
+        data_in_r <= 0;
+        last_in_r <= 0;
+        keep_in_r <= 0;
+    end
+    else if(ready_out)begin
+        if(valid_in&&valid_insert)begin
+            data_in_r <= data_in;
+            last_in_r <= last_in;
+            keep_in_r <= keep_in;
+        end
+        else begin
+            data_in_r <= 0;
+            last_in_r <= 0;
+            keep_in_r <= 0;
+        end
+    end
+    else begin
+        data_in_r <= data_in_r;
+        last_in_r <= last_in_r;
+        keep_in_r <= keep_in_r;
+    end    
+end
+
+/*-----------------------------------------------------data_out------------------------------------------------------*/
+
+assign data_out = data_out_r;
+assign valid_out = valid_out_r || last_out_r;
+assign keep_out = keep_out_r;
+assign last_out = last_out_r;
+
+always @(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin
+        data_out_r <= 0;
+        keep_out_r <= 0;
+        last_out_r <= 0;
+    end
+    else if (ready_out) begin
+            case({keep_insert_r,keep_in_r})
+            8'b0001_1000:begin
+                data_out_r <= {data_insert_r[7:0],data_in_r[31:24],{2{8'h00}}};    
+                keep_out_r <= 4'b1110;
+                last_out_r <= 1;
+            end 
+            8'b0001_1100:begin
+                data_out_r <= {data_insert_r[7:0],data_in_r[31:16],{8'h00}};    
+                keep_out_r <= 4'b1110;
+                last_out_r <= 1;
+            end          
+            8'b0001_1110:begin
+                data_out_r <= {data_insert_r[7:0],data_in_r[31:8]};    
+                keep_out_r <= 4'b1111;
+                last_out_r <= 1;
+            end
+            8'b0001_1111:begin
+                data_out_r <= {data_insert_r[7:0],data_in_r[31:8]};    
+                keep_out_r <= 4'b1111;
+                if(last_in_r) 
+                    last_out_r <= 1;
+                else 
+                    last_out_r <= 0;
+            end
+            8'b0011_1000:begin
+                data_out_r <= {data_insert_r[15:0],data_in_r[31:24],{2{8'h00}}};    
+                keep_out_r <= 4'b1110;
+                last_out_r <= 1;
+            end 
+            8'b0011_1100:begin
+                data_out_r <= {data_insert_r[15:0],data_in_r[31:16]};    
+                keep_out_r <= 4'b1111;
+                last_out_r <= 1;
+            end          
+            8'b0011_1110:begin
+                data_out_r <= {data_insert_r[15:0],data_in_r[31:16]};    
+                keep_out_r <= 4'b1111;
+                last_out_r <= 1;
+            end
+            8'b0011_1111:begin
+                data_out_r <= {data_insert_r[15:0],data_in_r[31:16]};    
+                keep_out_r <= 4'b1111;
+                if(last_in_r)
+                    last_out_r <= 1;
+                else
+                    last_out_r <= 0;
+            end
+            8'b0111_1000:begin
+                data_out_r <= {data_insert_r[23:0],data_in_r[31:24]};    
+                keep_out_r <= 4'b1111;
+                last_out_r <= 1;
+            end
+            8'b0111_1100:begin
+                data_out_r <= {data_insert_r[23:0],data_in_r[31:24]};    
+                keep_out_r <= 4'b1111;
+                last_out_r <= 1;
+            end 
+            8'b0111_1110:begin
+                data_out_r <= {data_insert_r[23:0],data_in_r[31:24]};    
+                keep_out_r <= 4'b1111;
+                last_out_r <= 1;
+            end
+            8'b0111_1111:begin
+                data_out_r <= {data_insert_r[23:0],data_in_r[31:24]};    
+                keep_out_r <= 4'b1111;
+                if(last_in_r)
+                    last_out_r <= 1;
+                else
+                    last_out_r <= 0;
+            end
+            8'b1111_1000:begin
+                data_out_r <= data_insert_r;    
+                keep_out_r <= 4'b1111;
+                last_out_r <= 1;
+            end
+            8'b1111_1100:begin
+                data_out_r <= data_insert_r;    
+                keep_out_r <= 4'b1111;
+                last_out_r <= 1;
+            end 
+            8'b1111_1110:begin
+                data_out_r <= data_insert_r;    
+                keep_out_r <= 4'b1111;
+                last_out_r <= 1;
+            end
+            8'b1111_1111:begin
+                data_out_r <= data_insert_r;    
+                keep_out_r <= 4'b1111;
+                if(last_in_r)
+                    last_out_r <= 1;
+                else
+                    last_out_r <= 0;
+            end
+            default: begin
+                data_out_r <= 0;
+                keep_out_r <= 0;
+                last_out_r <= 0;
+            end                        
+            endcase           
+        end
+    else begin
+        data_out_r <= data_out_r;
+        keep_out_r <= keep_out_r;
+        last_out_r <= last_out_r;
+    end
+end
+
+always @(posedge clk  or negedge rst_n) begin
+    if(!rst_n) begin
+        valid_out_r <= 0;
+    end
+    else if(valid_insert&&valid_in)begin
+        if(!keep_insert_r)begin
+            valid_out_r <= 0;
+        end
+        else begin
+            valid_out_r <= 1;
+        end
+    end
+    else begin
+        valid_out_r <= 0;
+    end
+end
+endmodule
---
+++ axi_stream_insert_header_tb.v
@@ -0,0 +1,223 @@
+`timescale 1ns/1ns
+module tb_axi_stream_insert_header (); 
+	reg rst_n;
+	reg clk;
+	
+	initial begin
+		clk = '0;
+		forever #(5) clk = ~clk;
+	end
+
+	initial begin
+		rst_n <= 0;
+		#10 rst_n <= 1;
+	end
+
+	// 收发变量
+	parameter 					DATA_WD = 32;
+	parameter 					DATA_BYTE_WD = DATA_WD / 8;
+
+	reg 					    valid_in;
+	reg [DATA_WD-1 : 0] 		data_in;
+	reg [DATA_BYTE_WD-1 : 0] 	keep_in;
+	reg 						last_in;
+	wire 						ready_in;
+
+	reg  						valid_insert;
+	reg [DATA_WD-1 : 0] 		data_insert;
+	reg [DATA_BYTE_WD-1 : 0] 	keep_insert;
+	reg [1 : 0] 				byte_insert_cnt;
+	wire 						ready_insert;
+
+	wire 						valid_out;
+	wire [DATA_WD-1 : 0] 		data_out;
+	wire [DATA_BYTE_WD-1 : 0] 	keep_out;
+	wire 						last_out;
+	reg 						ready_out;
+
+	integer i;
+
+	axi_stream_insert_header #(
+			.DATA_WD			(DATA_WD),
+			.DATA_BYTE_WD		(DATA_BYTE_WD)
+		) inst_axi_stream_insert_header (
+			.clk 				(clk),
+			.rst_n 				(rst_n),
+			.valid_in 			(valid_in),
+			.data_in 			(data_in),
+			.keep_in 			(keep_in),
+			.last_in 			(last_in),
+			.ready_in 			(ready_in),
+			.valid_insert 		(valid_insert),
+			.data_insert 		(data_insert),
+			.keep_insert 		(keep_insert),
+			.ready_insert 		(ready_insert),
+			.byte_insert_cnt	(byte_insert_cnt),
+			.valid_out 			(valid_out),
+			.data_out 			(data_out),
+			.keep_out 			(keep_out),
+			.last_out 			(last_out),
+			.ready_out			(ready_out)
+		);
+
+	task init();begin
+		valid_in 				<= 0;
+		data_in 				<= 0;
+		keep_in 				<= 0;
+		last_in 				<= 0;
+		valid_insert 			<= 0;
+		data_insert 			<= 0;
+		keep_insert 			<= 0;
+		byte_insert_cnt 		<= 0;
+		ready_out 				<= 0;	
+		@(posedge clk);
+		ready_out 				<= 1;
+		valid_insert 			<= 1;
+		valid_in 				<= 1;
+		repeat(4)@(posedge clk);
+	end
+	endtask
+
+	task data_unready();begin
+		keep_in       		<= keep_in;
+		last_in       		<= last_in;
+		data_in       		<= data_in;
+	end
+	endtask
+
+	task data_ready(input last_n);begin
+		keep_in       <= 4'b1111;
+		last_in       <= last_n;
+		data_in       <= {$random};
+	end
+	endtask
+
+	task insert_unready();begin
+		data_insert 		<= data_insert;
+		keep_insert  		<= keep_insert;
+		byte_insert_cnt 	<= byte_insert_cnt;
+	end
+	endtask
+
+	task insert_ready();begin
+		data_insert   	   <= {$random};
+		keep_insert   	   <= 4'b0111;
+		byte_insert_cnt    <= byte_insert_cnt;
+	end
+	endtask
+
+	task driver();begin
+		case ({ready_in,ready_insert}) 
+		2'b00: begin
+			data_unready();
+			insert_unready();
+			@(posedge clk);
+		end	
+		2'b01: begin
+			data_unready();
+			insert_ready();
+			@(posedge clk);
+		end
+		2'b11: begin
+			data_ready(0);
+			insert_ready();
+			@(posedge clk);
+		end
+		2'b10: begin
+			data_ready(0);
+			insert_unready();
+			@(posedge clk);
+		end
+		default: begin
+			data_ready(0);
+			insert_ready();
+			@(posedge clk);
+		end			
+		endcase	
+	end
+	endtask
+
+	task driver_last();begin
+		case ({ready_in,ready_insert}) 
+		2'b00: begin
+			data_unready();
+			insert_unready();
+			@(posedge clk);
+		end	
+		2'b01: begin
+			data_unready();
+			insert_ready();
+			@(posedge clk);
+		end
+		2'b11: begin
+			data_ready(1);
+			insert_ready();
+			@(posedge clk);
+		end
+		2'b10: begin
+			data_ready(1);
+			insert_unready();
+			@(posedge clk);
+		end
+		default: begin
+			data_ready(1);
+			insert_ready();
+			@(posedge clk);
+		end			
+		endcase	
+	end
+	endtask
+
+	task nextdata();begin
+		keep_in       <= 4'b1111;
+		last_in       <= 0;
+		data_in       <= {$random};
+		repeat(2)@(posedge clk);
+		valid_insert  <= 0;
+	end
+	endtask
+
+	task last2();begin
+		keep_in       <= 4'b1110;
+		last_in       <= 1;
+		data_in       <= {$random};
+		@(posedge clk);
+		last_in       <= 0;
+	end
+	endtask
+
+	task close();begin
+		valid_in  	  <= 0;
+		valid_insert  <= 0;
+		repeat(2)@(posedge clk);
+		ready_out     <= 0;
+		@(posedge clk);
+	end
+	endtask	
+
+	initial begin
+		init();
+		for (i=0; i< 6; i = i + 1) begin
+			driver();
+			if(i <= 3 && i >= 1)
+				ready_out <= 0;
+			else
+				ready_out <= 1;		
+		end
+		driver_last();
+
+		for (i=0; i< 6; i = i + 1) begin
+			driver();			
+		end
+		driver_last();
+		close();
+	end
+
+	initial begin
+      forever begin
+         #100;
+         if ($time >= 1000)  $finish ;
+      end
+   end
+
+endmodule
